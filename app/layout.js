4aa72bfe6a03f017130deb8792a7a1ed3f13b381
ManicManChaos <eugenedidit8@gmail.com>
2026-01-14 10:28:30 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Manic Musings of Chaos",
+  themeColor: "#0B0F0D",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
