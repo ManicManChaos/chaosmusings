41640db4e246473a937ee1c7d46f28478a064f01
ManicManChaos <eugenedidit8@gmail.com>
2026-01-14 10:32:18 -0600

    Update Glyph.js

--- components/ui/Glyph.js
+++ components/ui/Glyph.js
@@ -1,1 +1,7 @@
-
+export default function Glyph({ name, label }) {
+  return (
+    <button className="runeBtn" type="button" aria-label={label}>
+      <img className="rune" src={`/icons/${name}.svg`} alt="" />
+    </button>
+  );
+}
