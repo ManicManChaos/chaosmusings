66c900524e5f1ae5506e3502d9bc2a7f064b1b33
ManicManChaos <eugenedidit8@gmail.com>
2026-01-17 17:02:05 -0600

    Create route.js

---
+++ src/app/api/auth/[…nextauth]/route.js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth/next";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+});
+
+export { handler as GET, handler as POST };
