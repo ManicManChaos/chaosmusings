3281e9c60f5b547fd5a5d2e298754c5a3a54491d
ManicManChaos <eugenedidit8@gmail.com>
2026-01-17 16:05:57 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
 import "./globals.css";
+import AuthProvider from "../components/AuthProvider";
 
 export const metadata = {
   title: "Manic Musings of Chaos",
-  description: "Manic Musings of Chaos",
-  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
