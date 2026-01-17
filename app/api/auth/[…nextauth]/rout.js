e8c10c09b1df14138cc0647e671f103e5a7bdc35
ManicManChaos <eugenedidit8@gmail.com>
2026-01-17 16:45:05 -0600

    Update rout.js

--- app/api/auth/[…nextauth]/rout.js
+++ app/api/auth/[…nextauth]/rout.js
@@ -1,22 +1,15 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  pages: {
-    signIn: "/login",
-  },
-  callbacks: {
-    async session({ session, token }) {
-      session.user.id = token.sub;
-      return session;
-    },
-  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
 });
 
 export { handler as GET, handler as POST };
